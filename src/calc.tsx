5bcb3c94cfad290e191f0b55fad0e04030775b15
Tyynekaisa <annakaisa.juhola@gmail.com>
2025-11-20 17:50:27 +0200

    project init

---
+++ src/calc.tsx
@@ -0,0 +1,20 @@
+// https://niisku.lab.fi/~tyynekaisa/calculator
+import React from 'react'
+import Container from '@mui/material/Container';
+
+export default function Calculator() {
+  return (
+    <>
+      {/* <Container maxWidth="sm">
+        
+      </Container> */}
+
+    
+   
+      <div className="calculator">
+        <h1>Simple Calculator</h1>
+        <p>Created by Kaisa Juhola using React</p>
+      </div>
+    </>
+  )
+}
